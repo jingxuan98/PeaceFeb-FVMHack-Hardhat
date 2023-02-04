6b860d76a07dbc61aa5de3ebfe4f4645f1a43b84
Zk <zklim95@hotmail.com>
2023-02-04 22:42:23 +0800

    deploy script fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,12 @@ async function main() {
 	console.log("Deploying contracts with the account:", deployer.address);
 	console.log("Account balance:", Math.round(ethers.utils.formatEther(await deployer.getBalance()) * 10000) / 10000);
   
-    const loanPoolContract = await ethers.getContractFactory("LoanPool");
+    const loanPoolContract = await ethers.getContractFactory("LoanPool", deployer);
     const loanPool = await loanPoolContract.deploy();
     await loanPool.deployed();
 	console.log("LoanPool address:", loanPool.address);
 
-    const treasuryContract = await ethers.getContractFactory("Treasury");
+    const treasuryContract = await ethers.getContractFactory("Treasury", deployer);
     const treasury = await treasuryContract.deploy(loanPool.address);
     await treasury.deployed();
 	console.log("Treasury address:", treasury.address);
@@ -18,10 +18,10 @@ async function main() {
 	await loanPool.initialize(treasury.address);
 	console.log("Initialized with address:", treasury.address);
 
-	await loanPool.fundPool({ value: ethers.utils.parseEther("0.1") });
-	console.log("Funded 0.1 FIL");
+	// await loanPool.fundPool({ value: ethers.utils.parseEther("0.1") });
+	// console.log("Funded 0.1 FIL");
 
-	console.log(ethers.utils.formatEther(await loanPool.getFundersAmount(deployer.address)));
+	// console.log(ethers.utils.formatEther(await loanPool.getFundersAmount(deployer.address)));
   }
   
   main()
